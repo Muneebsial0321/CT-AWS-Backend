26100eaa6e1838293f23c8e0ab65ff4eea8ac4f6
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2024-08-10 10:51:50 +0500

    cover image routes updated

--- Server.js
+++ Server.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(parser.json())
 app.use(cors({
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     origin: process.env.FRONT_URL, // Your frontend URL
     credentials: true
 }));
