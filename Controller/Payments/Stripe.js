f1f1e967d5e40076f56cc8e029d958b5ffcea657
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2024-10-12 13:13:27 +0500

    stripe fixed

--- Controller/Payments/Stripe.js
+++ Controller/Payments/Stripe.js
@@ -39,58 +39,62 @@ const userAgent = async (req, res) => {
   res.json({userAgent:req.headers['user-agent']})
 }
 const paymentByStripe = async (req, res) => {
+  const event = await Event.get(req.body.eventId);
+  const seller = await User.get(event.eventCreatedBy);
+  // const products = Object.entries(req.body.eventTicketArray).map(([name_,quantity]) => ({ name_, quantity }));
+  const products = req.body.eventTicketArray.map((e)=>{
+    const key = Object.keys(e)[0]
+    const value = e[Object.keys(e)[0]]
+    console.log({[key]:value})
+    return {
+      name_:key,
+      quantity:value}
 
-    const event = await Event.get(req.body.eventId);
-    const seller = await User.get(event.eventCreatedBy);
-    // const products = req.body.ticketArray
-    // const products = Object.entries(req.body.ticketArray)
-    const products = Object.entries(req.body.ticketArray).map(([name_,quantity]) => ({ name_, quantity }));
-
-    const eventID = req.body.eventId ;
-    const sellerID = seller.Users_PK;
-    const buyerID = req.body.buyerId;
-
-   
-    // console.log({token: jwtDataCreater({products,
-    //   sellerID,
-    //   eventID,
-    //   buyerID,
-    // })})
-    
-    // if (!ticket) {
-    //   return res.status(404).json({ error: 'Event not found' });
-    // } 
-  
-    try {
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items:getLineItems(products,event),
-        mode: 'payment',
-        // success_url: `${process.env.FRONT_URL}/paymentSuccess?token=${jwtDataCreater({products,sellerID,eventID,buyerID,})}`, // Redirect after successful payment
-        success_url: `${process.env.FRONT_URL}/ticketdetails}`, // Redirect after successful payment
-        cancel_url: `${process.env.FRONT_URL}/paymentfailed`, // Redirect after canceled payment
-        payment_intent_data: {
-          transfer_data: {
-            destination: 'acct_1Ppi8ZRtoFwfpEcp', // Seller's Stripe Account ID
-          },
-          metadata: {
-            ticketEventId:eventID, 
-            ticketSellerId: sellerID,
-            ticketSellerEmail:seller.email,
-            ticketBuyerId: buyerID
-          },
-     
-        },
-      });
-      res.json({
-        sessionId: session.id,
-         url: session.url 
-        // Return the session ID to the client
-      });
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({ error: error.message });
+  })
+  const eventTicketArray=event.eventTicketArray.map((e)=>{
+    const key = e.ticketType
+    const value = e.quantity
+    return {
+      [key]:value
     }
+  })
+  res.json({products,eventTicketArray})
+
+  
+    // const eventID = req.body.eventId ;
+    // const sellerID = seller.Users_PK;
+    // const buyerID = req.body.buyerId;
+  
+    // try {
+    //   const session = await stripe.checkout.sessions.create({
+    //     payment_method_types: ['card'],
+    //     line_items:getLineItems(products,event),
+    //     mode: 'payment',
+    //     // success_url: `${process.env.FRONT_URL}/paymentSuccess?token=${jwtDataCreater({products,sellerID,eventID,buyerID,})}`, // Redirect after successful payment
+    //     success_url: `${process.env.FRONT_URL}/ticketdetails}`, // Redirect after successful payment
+    //     cancel_url: `${process.env.FRONT_URL}/paymentfailed`, // Redirect after canceled payment
+    //     payment_intent_data: {
+    //       transfer_data: {
+    //         destination: 'acct_1Ppi8ZRtoFwfpEcp', // Seller's Stripe Account ID
+    //       },
+    //       metadata: {
+    //         ticketEventId:eventID, 
+    //         ticketSellerId: sellerID,
+    //         ticketSellerEmail:seller.email,
+    //         ticketBuyerId: buyerID
+    //       },
+     
+    //     },
+    //   });
+    //   res.json({
+    //     sessionId: session.id,
+    //      url: session.url 
+    //     // Return the session ID to the client
+    //   });
+    // } catch (error) {
+    //   console.log(error)
+    //   res.status(500).json({ error: error.message });
+    // }
   }
   
 module.exports = {paymentByStripe,userAgent}
