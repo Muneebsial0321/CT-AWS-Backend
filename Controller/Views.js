a018ee1082d901c363b509d4d2addacce0f4543a
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2024-10-14 20:46:21 +0500

    watch history updated

--- Controller/Views.js
+++ Controller/Views.js
@@ -1,4 +1,7 @@
 const Views = require('../Schemas/Views')
+const Videos = require('../Schemas/Videos')
+const Reviews = require('../Schemas/Reviews')
+const User = require('../Schemas/User')
 const { v4: uuidv4 } = require('uuid');
 
 const createView =async(req,res)=>{
@@ -34,9 +37,17 @@ const getUserWatchList =async(req,res)=>{
         .exec()
         const event= view.filter((e)=>e.viewItemType=='event')
         const video= view.filter((e)=>e.viewItemType=='video')
+        const data= await Promise.all(video.map(async(e)=>{
+            const vid = await Videos.get(e._id)
+            const com = await Reviews.scan('reviewItemId').eq(e._id).exec()          
+                // const { password, ...user } = await User.get(vid.userId);
+                return { data: vid, commments: com }
+            }
+          ))
         const podcast= view.filter((e)=>e.viewItemType=='podcast')
         const job= view.filter((e)=>e.viewItemType=='job')
-        res.json({event,video,podcast,job})
+        res.json({video,podcast})
+        // res.json({video:data,podcast})
     } catch (error) {
         console.log({error})
         res.send(error)
