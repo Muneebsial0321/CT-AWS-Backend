90c700bd9a688379dd71cc08973291246433fa73
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2024-10-15 10:12:16 +0500

    stripe fixed

--- Controller/Payments/Stripe.js
+++ Controller/Payments/Stripe.js
@@ -13,21 +13,28 @@ const jwtDataCreater = (data) => {
   const token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: '1h' });
   return token
 }
+
+const ticketPrice=(ticketName,ticketArray)=>{
+  console.log({ticketName,ticketArray})
+  const ticket = ticketArray.filter((e)=>e.ticketType==ticketName)
+  console.log({ticket})
+  console.log({price:ticket[0].price})
+  return ticket[0].price
+}
 const getLineItems = (proArray, event) => {
   // prodArray will have ticket name with there quantity.....
   //meanwhile event will have event object
   // it shall return lineitems
   const lineItems = proArray.map((product) => ({
     price_data: {
-      currency: 'usd', // Use the appropriate currency
+      currency: 'usd', 
       product_data: {
-        name: product.name_, // Dynamically set the product name
+        name: product.name_, 
         description: event._id
       },
-      unit_amount: event[product.name_] * 100, // Stripe expects the amount in cents (for USD)
-      // unit_amount: product.price * 100, // Stripe expects the amount in cents (for USD)
+      unit_amount: ticketPrice(product.name_,event.eventTicketArray) * 100, 
     },
-    quantity: product.quantity, // Quantity of the product
+    quantity: product.quantity,
   }));
 
   const filteredData = lineItems.filter((e) => e.quantity != 0)
@@ -39,29 +46,28 @@ const userAgent = async (req, res) => {
   res.json({ userAgent: req.headers['user-agent'] })
 }
 const paymentByStripe = async (req, res) => {
+  console.log({"reponse":req.body})
   const event = await Event.get(req.body.eventId);
   const seller = await User.get(event.eventCreatedBy);
-  // const products = Object.entries(req.body.eventTicketArray).map(([name_,quantity]) => ({ name_, quantity }));
-  const products = req.body.eventTicketArray.map((e) => {
-    const key = Object.keys(e)[0]
-    const value = e[Object.keys(e)[0]]
-    console.log({ [key]: value })
-    return {
-      name_: key,
-      quantity: value
-    }
+  const products = Object.entries(req.body.eventTicketArray).map(([name_,quantity]) => ({ name_, quantity }));
+  // const products = req.body.eventTicketArray.map((e) => {
+  //   const key = Object.keys(e)[0]
+  //   const value = e[Object.keys(e)[0]]
+  //   // console.log({ [key]: value })
+  //   return {
+  //     name_: key,
+  //     quantity: value
+  //   }
 
-  })
-  const eventTicketArray = event.eventTicketArray.map((e) => {
-    const key = e.ticketType
-    const value = e.quantity
-    return {
-      [key]: value
-    }
-  })
-  // res.json({ products, eventTicketArray })
-
-
+  // })
+    // const eventTicketArray = event.eventTicketArray.map((e) => {
+  //   const key = e.ticketType
+  //   const value = e.quantity
+  //   return {
+  //     [key]: value
+  //   }
+  // })
+  // res.json({ products, eventTicketArray,lineItems:getLineItems(products,event) })
   const eventID = req.body.eventId ;
   const sellerID = seller.Users_PK;
   const buyerID = req.body.buyerId;
@@ -69,17 +75,17 @@ const paymentByStripe = async (req, res) => {
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
-      line_items:[ {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: "Product name",
-          },
-          unit_amount: 100 * 100,
-        },
-        quantity: 10,
-      }],
-      // line_items:getLineItems(products,event),
+      // line_items:[ {
+      //   price_data: {
+      //     currency: 'usd',
+      //     product_data: {
+      //       name: "Product name",
+      //     },
+      //     unit_amount: 100 * 100,
+      //   },
+      //   quantity: 10,
+      // }],
+      line_items:getLineItems(products,event),
       mode: 'payment',
       // success_url: `${process.env.FRONT_URL}/paymentSuccess?token=${jwtDataCreater({products,sellerID,eventID,buyerID,})}`, // Redirect after successful payment
       success_url: `${process.env.FRONT_URL}/ticketdetails`, // Redirect after successful payment
@@ -100,7 +106,6 @@ const paymentByStripe = async (req, res) => {
     res.json({
       sessionId: session,
       // url: session.url
-      // Return the session ID to the client
     });
   } catch (error) {
     console.log(error)
